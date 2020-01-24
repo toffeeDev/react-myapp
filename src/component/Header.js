48c9457ead5f03c28d1b09cb6d7c80bef9209bc6
ToffeeDev <sakanrin.top21@gmail.com>
2020-01-24 11:04:35 +0700

    react shapter 4

---
+++ src/component/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+
+  componentDidMount() {
+    console.log("Header: didMount");
+  }
+
+
+  componentWillReceiveProps(nextProps) {
+    console.log("nextProps", nextProps.like, "props", this.props.like);
+  }
+
+  render() {
+    console.log("Header: render");
+    return (
+      <div>
+        <h1>{this.props.children}</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
